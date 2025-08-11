e52396207fc9b38ae9571cfa85e1566f973123f0
Lownzito <103516646+Lownzito@users.noreply.github.com>
2025-08-11 18:52:29 -0300

    correções aula 1

--- Unidade1/Aula1/src/App.java
+++ Unidade1/Aula1/src/App.java
@@ -1,9 +1,26 @@
 public class App {
+    /**
+     * ignorar, não faz parte da aula 1
+     * 
+     */
     public static String variavel = "10";
     public static String variavel2 = "10";
 
     public static void main(String[] args) throws Exception 
     {
+        System.out.println("ola mundo");
+
+        System.out.println("Teste");
+        int valorA = 5;
+        int valorB = 10;
+
+        int resultado = valorA + valorB;
+
+        System.out.println("Resultado é:" + resultado);
+
+        /**
+         * ignorar esse código, ele não faz parte da aula 1
+         */
         variavel2 += variavel;
         System.out.println("A variavel tem um valor de:" + (variavel2));
     }
