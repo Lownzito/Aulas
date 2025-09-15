6227fb1261f997e8b29e0249ad7d57e742bacddc
Lownzito <103516646+Lownzito@users.noreply.github.com>
2025-09-15 19:09:41 -0300

    Update CalcFurb.java

--- Unidade4/src/CalcFurb.java
+++ Unidade4/src/CalcFurb.java
@@ -1,12 +1,23 @@
 import java.util.Scanner;
 
 public class CalcFurb {
-    public CalcFurb() {
-        double numero1, numero2, calc;
-        int opcao;
-        
-        Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
+    double numero1, numero2;
+    int opcao;
+
+    public CalcFurb() {
+
+        inputValues();
+
+        sc.close();
+    }
+
+    public static void main(String[] args) {
+        new CalcFurb();
+    }
+
+    private void inputValues() {
         System.out.println("Bem vindo ao Calc Furb!!");
 
         System.out.println("Agora informe dois números");
@@ -16,6 +27,10 @@ public class CalcFurb {
         System.out.println("Informe o segundo número: ");
         numero2 = sc.nextDouble();
 
+        selectOption();
+    }
+
+    private void selectOption() {
         System.out.println("Agora selecione uma opção");
         System.out.println("Se for selecionado errado, será somado.");
         System.out.println("1 - Somar");
@@ -28,33 +43,72 @@ public class CalcFurb {
 
         opcao = sc.nextInt();
 
-        runSelectedOption(opcao, numero1, numero2);
-
-        sc.close();
-
+        runSelectedOption();
     }
 
-    public static void main(String[] args) {
-        new CalcFurb();
-    }
-
-    public static void runSelectedOption(int opcao, double numero1, double numero2) {
+    private void runSelectedOption() {
         switch (opcao) {
             case 1:
-                System.out.println("O resultado da soma foi: " + (numero1 + numero2));
+                somar();
                 break;
             case 2:
-                System.out.println("O resultado da subtração foi: " + (numero1 - numero2));
+                subtrair();
                 break;
             case 3:
-                System.out.println("O resultado da multiplicação foi: " + (numero1 * numero2));
+                multiplicar();
                 break;
             case 4:
-                System.out.println("O resultado da divisão foi: " + (numero1 / numero2));
+                dividir();
                 break;
             default:
-                System.out.println("O resultado da soma foi: " + (numero1 + numero2));
+                System.out.println("Opção inválida, selecione uma das opções disponiveis: ");
+                selectOption();
                 break;
         }
+
+        shouldContinue();
     }
+
+    private void shouldContinue() {
+        System.out.println("Você gostaria de continuar?");
+        System.out.println("Sim - 1");
+        System.out.println("Não - 2");
+
+        int shouldContinue = sc.nextInt();
+
+        if (shouldContinue == 1) {
+            inputValues();
+        } else if(shouldContinue == 2) {
+            System.out.println("Tchau, tenha um bom dia!");
+        }
+        else{ 
+            System.out.println("Insira uma opção válida!");
+            shouldContinue();
+        }
+    }
+
+    private void printResult(double result) {
+        System.out.println("O resultado foi: " + result);
+    }
+
+    private void somar() {
+        printResult((numero1 + numero2));
+    }
+
+    private void subtrair() {
+        printResult((numero1 - numero2));
+    }
+
+    private void multiplicar() {
+        printResult((numero1 * numero2));
+    }
+
+    private void dividir() {
+        if (numero2 > 0) {
+            printResult((numero1 / numero2));
+        } else {
+            printResult(0);
+        }
+    }
+
 }
